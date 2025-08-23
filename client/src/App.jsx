f6e1325fd1134af0b21387d5772f4c757d5f9e4b
annisanursalamah29@gmail.com <annisanursalamah29@gmail.com>
2025-08-23 11:10:24 +0700

    kelima

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import StudentList from './components/StudentList';
 import StudentForm from './components/StudentForm';
 import './App.css';
 
-const API_URL = 'http://localhost:5000/api/students';
+const API_URL = "https://mern-crud-smoky-seven.vercel.app/api/students";
 
 function App() {
   const [students, setStudents] = useState([]);
