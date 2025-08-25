dabd094d35650bf85e60ada8274a3c2729ac62b0
annisanursalamah29@gmail.com <annisanursalamah29@gmail.com>
2025-08-25 15:41:49 +0700

    ke14

--- server/server.js
+++ server/server.js
@@ -104,7 +104,12 @@ app.delete("/api/students/:id", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
